bc91dde24aa765dcee35336f79ae9deda791a73f
Satyam-Singh-x <singhsatyam.0912@gmail.com>
2025-08-14 11:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import streamlit as st
 import pickle
 import helper
+import zipfile
+import os
 
-# ------------------ Load Model ------------------ #
-model = pickle.load(open('model.pkl', 'rb'))
+
+# ------------------ Paths ------------------ #
+zip_path = "model.zip"         # Your ZIP file containing model.pkl
+extract_path = "model_folder"  # Folder to extract
+
+# ------------------ Unzip the model ------------------ #
+os.makedirs(extract_path, exist_ok=True)
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
+
+# ------------------ Load the model ------------------ #
+model_path = os.path.join(extract_path, "model.pkl")
+model = pickle.load(open(model_path, "rb"))
+
 
 # ------------------ Page Config ------------------ #
 st.set_page_config(
@@ -62,5 +76,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
 
